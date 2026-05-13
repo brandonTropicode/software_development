74e7f58e6a2327d5dd0fa92c830a5f4a833acf5c
Brandon <brandon@Boriss-Mac-mini.local>
2026-05-13 16:43:21 -0400

    webcalculator backend done

--- projects/web_calculator/website.py
+++ projects/web_calculator/website.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from schemas import NumInput
 
 # our server
@@ -20,24 +20,39 @@ def home():
 def add(data:NumInput):
     sum = data.num1 + data.num2
     return {
-        "opperation":"add",
+        "operation":"add",
         "result":sum
     }
 
 # subtract route
 @bob_server.post('/subtract')
-def subtract():
-    pass
+def subtract(data:NumInput):
+    difference = data.num1 - data.num2
+    return {
+        "operation":"subtract",
+        "result":difference
+    }
     
 # multiply route
 @bob_server.post('/multiply')
-def multiply():
-    pass
+def multiply(data:NumInput):
+    product = data.num1 * data.num2
+    return {
+        "operation":"multiply",
+        "result":product
+    }
 
 # divide route
 @bob_server.post('/divide')
-def divide():
-    pass
+def divide(data:NumInput):
+    if data.num2 == 0:
+        #when user error code 400/when system error 500
+        raise HTTPException(status_code = 400,detail = "Stop dividing by 0 por favor")
+    quotiont = data.num1/data.num2
+    return {
+        "operation":"divide",
+        "result":quotiont
+    }
 
 # command to run your server
 # uvicorn website:bob_server --reload
